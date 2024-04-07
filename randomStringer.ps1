322e7c872b8f7d0b63c46d9cfc2964e057c89311
iwouldratherbeatthebeach <66068624+iwouldratherbeatthebeach@users.noreply.github.com>
2024-04-06 22:26:35 -0400

    Add files via upload

---
+++ randomStringer.ps1
@@ -0,0 +1,16 @@
+﻿clear host
+do {
+   $TokenSet = @{
+        L = [Char[]]'RANDOM STRING'
+    }
+
+$Lower = Get-Random -Count 13 -InputObject $TokenSet.L
+
+$StringSet =($Lower)
+
+[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+[System.Windows.Forms.SendKeys]::SendWait("$StringSet{ENTER}")
+
+Start-Sleep -Seconds 0.75
+
+} While ($true)
